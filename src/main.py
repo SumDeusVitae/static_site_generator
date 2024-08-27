397d823c30c273dc20159052778de5abd2a69379
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-08-27 13:43:55 -0500

    copy static

--- src/main.py
+++ src/main.py
@@ -1,10 +1,38 @@
+import os
+import shutil
 from textnode import TextNode
 from htmlnode import ParentNode, HTMLNode, LeafNode
-def main():
-    x = TextNode('This is a text node','bold')
-    y = TextNode('This is a text node','bold')
 
-    print(x)
-    print(x == y)
+def generate_output(source, destination):
+    if not os.path.exists(source):
+        raise ValueError(f"Source path doesn't exist: {source}")
+    if not os.path.exists(destination):
+        raise ValueError(f"Destination path doesn't exist: {destination}")
+    final_destination = os.path.join(destination, 'public')
+    if os.path.exists(final_destination):
+        shutil.rmtree(final_destination)
+    os.mkdir(final_destination)
+    copy_paste(source, final_destination)
+
+def copy_paste(source, destination):
+    files = os.listdir(source)
+    for file in files:
+        new_source = os.path.join(source, file)
+        new_destination = os.path.join(destination, file)
+        if os.path.isfile(new_source):
+            shutil.copy(new_source, new_destination)
+        else:
+            os.mkdir(new_destination)
+            copy_paste(new_source, new_destination)
+
+
+
+
+
+def main():
+    source  = "/home/deusvitae/workspace/github.com/SumDeusVitae/static_site_generator/static"
+    destination = "/home/deusvitae/workspace/github.com/SumDeusVitae/static_site_generator"
+    generate_output(source, destination)
+
 main()
 
